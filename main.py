23158b9e71737932b0038110901dc0e785189931
democodingal <mayurirastogi0@gmail.com>
2024-10-22 22:33:56 +0530

    done

---
+++ main.py
@@ -0,0 +1,12 @@
+#lets check the datatypes of different values
+a=5
+print("type of a= ", type(a))
+
+b=2.3
+print("type of b= ", type(b))
+
+c="codingal"
+print("type od c is= ",type(c))
+
+d=True
+print("type of d is= ",type(d))
